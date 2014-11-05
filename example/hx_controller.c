77e6905842b329a0a240ecc152bcb51135f0d8cb
Carlos Agüero <caguero@osrfoundation.org>
2014-11-04 18:06:11 -0800

    Adding stdafx.h to the hx_controller.c example.

--- example/hx_controller.c
+++ example/hx_controller.c
@@ -15,18 +15,18 @@
  *
 */
 
+#ifdef _MSC_VER
+# include "stdafx.h"
+#endif
 #ifdef _WIN32
-#include <windows.h>
-#define _USE_MATH_DEFINES
+# include <windows.h>
+# define _USE_MATH_DEFINES
 #endif
 #include <math.h>
 #include <signal.h>
 #include <stdio.h>
 #include <time.h>
 #include <haptix/comm/haptix.h>
-#ifdef _WIN32
-#include <windows.h>
-#endif
 
 int running = 1;
 
