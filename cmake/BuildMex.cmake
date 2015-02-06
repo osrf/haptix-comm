a09ba6de067022a92d1c13894baeb1a528fe0cfe
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-02-06 02:13:43 +0100

    Use MATLAB in same way than Octave

--- cmake/BuildMex.cmake
+++ cmake/BuildMex.cmake
@@ -12,19 +12,12 @@ if(NOT MATLAB_MEX_PATH)
 endif()
 
 if(NOT MATLAB_MEX_PATH)
-  message(STATUS "MATLAB mex compiler not found - no mex file generation")
+  set (MATLAB_FOUND 0)
   BUILD_WARNING("MATLAB mex compiler not found - no mex file generation")
 else()
+  set (MATLAB_FOUND 1)
   message(STATUS "MATLAB mex compiler found")
   find_path(MATLAB_INCLUDE_DIR
      "mex.h"
      ${MATLAB_ROOT}/extern/include)
 endif()
-
-macro (build_mex_file)
-  # CMake 2.8.12 & earlier apparently don't define the
-  # Mex script path, so find it.
-   execute_process(COMMAND ${MATLAB_MEX_PATH} ${SOURCE_FILE} -I${CMAKE_BINARY_DIR} -L${CMAKE_BINARY_DIR}/src -L${CMAKE_BINARY_DIR}/msg -lhaptix-comm  -lhaptix-mgs -L${PROTOBUF_SRC_ROOT_FOLDER}/include -l${PROTOBUF_LIBRARY} -L${ZeroMQ_libzmq_LIBDIR} -l${ZeroMQ_LIBRARIES} -I${ignition-transport_DIR} -L${ignition-transport_DIR} -lignition-transport -lws2_32 -lIphlpapi
-    OUTPUT_VARIABLE mexOut
-    ERROR_VARIABLE mexErr)
-endmacro()
