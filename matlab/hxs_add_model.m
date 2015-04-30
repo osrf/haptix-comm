61aa2ed1382bdb5962599368dee6a5e3e79ab3c3
Brian Gerkey <gerkey@osrfoundation.org>
2015-04-30 12:04:33 -0700

    adding docs

--- matlab/hxs_add_model.m
+++ matlab/hxs_add_model.m
@@ -1,9 +1,20 @@
-%ADD_MODEL
+%ADD_MODEL Add model during runtime.
 %
-% result = add_model()
+% model = add_model(sdf, name, pos, orient, gravity_mode)
 %
-%TODO: document
+% Parameters:
+%   sdf (string) : SDF xml description of the model.
+%   name (string) : Model name.
+%   pos (float array) : A 3x1 (or 1x3) array (X,Y,Z) of model position in the
+%     global frame (m).
+%   orient (float array) : A 3x1 (or 1x3) array (roll, pitch, yaw) of model
+%     orientation in the global frame (rad).
+%   gravity_mode (bool) : true if the model is affected by gravity, 0 otherwise.
+% 
+% Return values:
+%   model: Structure with the following named fields:
+%     TODO    
 
-function result = hxs_add_model()
-  result = hxgz("add_model");
+function result = hxs_add_model(sdf, name, pos, orient, gravity_mode)
+  result = hxgz("add_model", sdf, name, pos, orient, gravity_mode);
 end
