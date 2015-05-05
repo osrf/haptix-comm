2ebaf6988e1d3e2d3710858398c18dd69c92dafa
Brian Gerkey <gerkey@osrfoundation.org>
2015-05-05 16:38:46 -0700

    filling out example

--- matlab/hxs_example.m
+++ matlab/hxs_example.m
@@ -12,6 +12,12 @@ for model_idx = 1:size(info.models)
     link = model.links(link_idx).link;
     disp(link.name);
   end
+  disp("Joints:");
+  for joint_idx = 1:size(model.joints)
+    joint = model.joints(joint_idx).joint;
+    % TODO: this doesn't work yet
+    %disp(joint.name);
+  end
 end
 
 % Get the user camera pose
@@ -44,11 +50,11 @@ vel = hxs_linear_velocity("wood_cube_5cm");
 disp(vel);
 hxs_apply_force("wood_cube_5cm", "link", [-1.0; 0; 0], 0.2);
 % Let it get moving
-sleep(0.1)
+sleep(0.1);
 vel = hxs_linear_velocity("wood_cube_5cm");
 disp(vel);
 % Let it settle
-sleep(1.5)
+sleep(1.5);
 vel = hxs_linear_velocity("wood_cube_5cm");
 disp(vel);
 
@@ -59,19 +65,52 @@ vel = hxs_angular_velocity("wood_cube_5cm");
 disp(vel);
 hxs_apply_torque("wood_cube_5cm", "link", [0; 0; 0.1], 0.1)
 % Let it get moving
-sleep(0.1)
+sleep(0.1);
 vel = hxs_angular_velocity("wood_cube_5cm");
 disp(vel);
 % Let it settle
-sleep(1.5)
+sleep(1.5);
 vel = hxs_angular_velocity("wood_cube_5cm");
 disp(vel);
 
-%% Apply torque to the small wooden cube, moving it sideways
-hxs_apply_force("wood_cube_5cm", "link", [-1.0; 0; 0], 0.1);
-% Query linear velocity
-ang_vel = hxs_angular_velocity("wood_cube_5cm");
-disp(ang_vel);
+% Apply force and torque at the same time.
+wrench = struct("force", [0; 0; 1], "torque", [0; 0; 0.1])
+hxs_apply_wrench("wood_cube_5cm", "link", wrench, 0.1);
+sleep(1.5);
 
-% Get the angular velocity to of the wooden cube
-hxs_angular_velocity("wood_cube_5cm")
+% Check gravity mode on wooden cube
+gravity_mode = hxs_model_gravity_mode("wood_cube_5cm");
+disp(gravity_mode);
+% Turn off gravity for cube, then nudge it upward
+hxs_set_model_gravity_mode("wood_cube_5cm", 0);
+hxs_apply_force("wood_cube_5cm", "link", [0; 0; 0.1], 0.1);
+% Let if tly
+sleep(1);
+% Bring it back down
+hxs_set_model_gravity_mode("wood_cube_5cm", gravity_mode);
+
+% Get the pose of the cube
+tx = hxs_model_transform("wood_cube_5cm");
+disp("Cube position:");
+disp(tx.pos);
+disp("Cube orientation:");
+disp(tx.orient);
+
+% Check collide mode on the cube
+collide_mode = hxs_model_collide_mode("wood_cube_5cm");
+disp(collide_mode);
+% Let it drop through the world
+hxs_set_model_collide_mode("wood_cube_5cm", 0);
+% Hack: apply a small force to disturb the cube to make it actually fall.
+hxs_apply_force("wood_cube_5cm", "link", [0; 0; 0.1], 0.1);
+sleep(1);
+% Turn collisions back on (won't bring the cube back, of course)
+hxs_set_model_gravity_mode("wood_cube_5cm", collide_mode);
+
+% Add a new model
+% TODO
+% Remove the model
+% TODO
+
+% Set the state of a wrist joint
+%hxs_set_model_joint_state("mpl_haptix_right_forearm", 
