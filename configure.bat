732c9adb7a719fbe96a3cae56afb0a2a6c86b89d
Carlos Agüero <caguero@osrfoundation.org>
2015-05-26 14:54:30 -0700

    Spread long cmake lines in configure.bat over multiple lines.

--- configure.bat
+++ configure.bat
@@ -14,6 +14,14 @@
 @if "%build_bitness%"=="32" set MATLAB_PATH=C:\Program Files (x86)\MATLAB\R2014b
 
 @echo Configuring for build type %build_type%
-cmake -DCMAKE_PREFIX_PATH="..\ign-transport\build\install\%build_type%" -DPROTOBUF_SRC_ROOT_FOLDER="..\..\protobuf-2.6.0-win%build_bitness%-vc12" -DCPPZMQ_HEADER_PATH="..\cppzmq" -DZeroMQ_ROOT_DIR="..\ZeroMQ 3.2.4" -DMATLAB_ROOT:STRING="%MATLAB_PATH%" -DCMAKE_INSTALL_PREFIX="install/%build_type%" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE="%build_type%" ..
+cmake -DCMAKE_PREFIX_PATH="..\ign-transport\build\install\%build_type%"^
+      -DPROTOBUF_SRC_ROOT_FOLDER="..\..\protobuf-2.6.0-win%build_bitness%-vc12"^
+      -DCPPZMQ_HEADER_PATH="..\cppzmq"^
+      -DZeroMQ_ROOT_DIR="..\ZeroMQ 3.2.4"^
+      -DMATLAB_ROOT:STRING="%MATLAB_PATH%"^
+      -DCMAKE_INSTALL_PREFIX="install/%build_type%"^
+      -G "NMake Makefiles"^
+      -DCMAKE_BUILD_TYPE="%build_type%"^
+      ..
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @echo Configuration complete.  To build, run `nmake`
