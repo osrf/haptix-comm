d65f4b40788ad2f07e73f35ed6061b8f243af41e
Carlos Agüero <caguero@osrfoundation.org>
2016-04-25 19:42:19 -0700

    Splitting

--- example/validationController.cc
+++ example/validationController.cc
@@ -21,231 +21,216 @@
 #include <gazebo/common/Timer.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <ignition/transport/Node.hh>
+#include "validationController.hh"
 
-/// \brief Labels for states.
-static std::string kReadyState  = "controller_ready";
-static std::string kInitialConditionsState  = "controller_initial_conditions";
-
-// Forward declarations.
-class ValidationController;
-
-/// \brief State pattern used for the state machine.
-class State
+/////////////////////////////////////////////////
+State::State(const std::string &_name,
+             ValidationController &_plugin)
+  : name(_name),
+    plugin(_plugin),
+    publicationInterval(1.0)
 {
-  /// \brief Class constructor.
-  /// \param[in] _name Name of the state.
-  /// \param[in] _plugin Pointer to the model plugin.
-  public: State(const std::string &_name,
-                ValidationController &_plugin)
-    : name(_name),
-      plugin(_plugin),
-      publicationInterval(1.0)
-  {
-    // This is the topic where we receive the state from Gazebo.
-    this->node.Subscribe("/gazebo/validation/state", &State::OnState, this);
+  // This is the topic where we receive the state from Gazebo.
+  this->node.Subscribe("/gazebo/validation/state", &State::OnState, this);
 
-    // Advertise controller feedback.
-    this->node.Advertise<gazebo::msgs::GzString>("/gazebo/validation/feedback");
-  }
+  // Advertise controller feedback.
+  this->node.Advertise<gazebo::msgs::GzString>("/gazebo/validation/feedback");
+}
 
-  /// \brief Update the state.
-  public: void Update()
-  {
-    //std::lock_guard<std::mutex> lock(this->mutex);
-    if (!this->initialized)
-      this->Initialize();
+/////////////////////////////////////////////////
+void State::Update()
+{
+  //std::lock_guard<std::mutex> lock(this->mutex);
+  if (!this->initialized)
+    this->Initialize();
 
-    std::cout << this->name << std::endl;
-    std::cout << "State::Update()" << std::endl;
-    std::cout << timer.GetElapsed() << std::endl;
+  //std::cout << this->name << std::endl;
+  //std::cout << "State::Update()" << std::endl;
+  //std::cout << timer.GetElapsed() << std::endl;
 
-    this->DoUpdate();
+  this->DoUpdate();
 
-    // Time to publish the current state?
-    auto now = gazebo::common::Time::GetWallTime();
-    if (now - this->lastPublication >= this->publicationInterval)
-      this->PublishState();
-  }
-
-  /// \brief Called once before changing to a different state.
-  public: void Teardown()
-  {
-    //std::lock_guard<std::mutex> lock(this->mutex);
-    this->initialized = false;
-
-    std::cout << "State::Teardown()" << std::endl;
-    this->DoTeardown();
-
+  // Time to publish the current state?
+  auto now = gazebo::common::Time::GetWallTime();
+  if (now - this->lastPublication >= this->publicationInterval)
     this->PublishState();
-  }
+}
 
-  /// \brief Equal to operator.
-  /// \param[in] _state The state to compare against.
-  /// \return true if the state has the same name.
-  public: bool operator ==(const State &_state) const
-  {
-    return this->name == _state.name;
-  }
+/////////////////////////////////////////////////
+void State::Teardown()
+{
+  //std::lock_guard<std::mutex> lock(this->mutex);
+  this->initialized = false;
 
-  /// \brief Not equal to operator
-  /// \param[in] _state The state to compare against
-  /// \return true if the state doesn't have the same name.
-  public: bool operator !=(const State &_state) const
-  {
-    return !(*this == _state);
-  }
+  //std::cout << "State::Teardown()" << std::endl;
+  this->DoTeardown();
 
-  /// \brief Return the current Gazebo state value.
-  protected: std::string GazeboState() const
-  {
-    //std::lock_guard<std::mutex> lock(this->mutex);
-    return this->gazeboState;
-  }
+  this->PublishState();
+}
 
-  /// \brief ToDo.
-  private: virtual void DoInitialize(){};
+/////////////////////////////////////////////////
+std::string State::GazeboState() const
+{
+  //std::lock_guard<std::mutex> lock(this->mutex);
+  return this->gazeboState;
+}
 
-  /// \brief ToDo.
-  private: virtual void DoUpdate(){};
-
-  /// \brief ToDo.
-  private: virtual void DoTeardown(){};
-
-  /// \brief ToDo.
-  private: virtual void DoOnState(){};
-
-  /// \brief Initialize the state. Called once after a pause duration after
-  /// entering state.
-  private: void Initialize()
-  {
-    std::cout << "State::Initialize()" << std::endl;
-    this->initialized = true;
-    this->timer.Reset();
-    this->timer.Start();
-    this->DoInitialize();
-  }
-
-  /// \brief Callback that updates the Gazebo state member variable.
-  private: void OnState(const gazebo::msgs::GzString &_msg)
-  {
-    //std::lock_guard<std::mutex> lock(this->mutex);
-    this->gazeboState = _msg.data();
-
-    // Only if we're the active state.
-    if (this->initialized)
-      this->DoOnState();
-  }
-
-   /// \brief Publish the current state.
-  private: void PublishState()
-  {
-    gazebo::msgs::GzString msg;
-    msg.set_data(this->name);
-    this->node.Publish("/gazebo/validation/feedback", msg);
-    this->lastPublication = gazebo::common::Time::GetWallTime();
-  }
-
-  /// \brief Name of the state.
-  public: const std::string name;
-
-  /// \brief Pointer to the validation plugin.
-  protected: ValidationController &plugin;
-
-  /// \brief Timer to measure time in the current state.
-  protected: gazebo::common::Timer timer;
-
-  /// \brief Has initialized
-  protected: bool initialized = false;
-
-  /// \brief Last gazebo state message.
-  private: std::string gazeboState;
-
-  /// \brief Mutex to protect the gazebo state message.
-  private: mutable std::mutex mutex;
-
-  /// \brief Last time the we published the state.
-  private: gazebo::common::Time lastPublication;
-
-  /// \brief Elapsed time between state publications.
-  private: gazebo::common::Time publicationInterval;
-
-  /// \brief Transport node.
-  protected: ignition::transport::Node node;
+/////////////////////////////////////////////////
+void State::DoInitialize()
+{
 };
 
-/// \brief State that handles the "init" state.
-class ReadyState : public State
+/////////////////////////////////////////////////
+void State::DoUpdate()
 {
-  // Use class constructor from base class.
-  using State::State;
-
-  // Documentation inherited
-  public: virtual void DoOnState()
-  {
-    if (this->GazeboState() == "gazebo_waiting_initial_conditions")
-      this->plugin.SetState(*this->plugin.setupState);
-
-    std::cout << "InitiState::DoFeedback()" << std::endl;
-  };
 };
 
-/// \brief State that handles the "initial_conditions" state.
-class InitialConditionsState : public State
+/////////////////////////////////////////////////
+void State::DoTeardown()
 {
-  // Use class constructor from base class.
-  using State::State;
-
-  public: virtual void DoInitialize()
-  {
-    // Set the initial conditions.
-  }
-
-  public: virtual void DoUpdate()
-  {
-    // Check when the initial conditions are ready.
-  }
-
-  // Documentation inherited
-  public: virtual void DoOnState()
-  {
-    if (this->GazeboState() == "gazebo_waiting_initial_conditions")
-      this->plugin.SetState(*this->plugin.setupState);
-
-    std::cout << "InitiState::DoFeedback()" << std::endl;
-  };
 };
 
-class ValidationController
+/////////////////////////////////////////////////
+void State::DoOnState()
 {
-  /// \brief ToDo
-  public: ValidationController()
+};
+
+/////////////////////////////////////////////////
+void State::Initialize()
+{
+  //std::cout << "State::Initialize()" << std::endl;
+  this->initialized = true;
+  this->timer.Reset();
+  this->timer.Start();
+  this->DoInitialize();
+}
+
+/////////////////////////////////////////////////
+void State::OnState(const gazebo::msgs::GzString &_msg)
+{
+  //std::lock_guard<std::mutex> lock(this->mutex);
+  this->gazeboState = _msg.data();
+
+  // Only if we're the active state.
+  if (this->initialized)
+    this->DoOnState();
+}
+
+/////////////////////////////////////////////////
+void State::PublishState()
+{
+  gazebo::msgs::GzString msg;
+  msg.set_data(this->name);
+  this->node.Publish("/gazebo/validation/feedback", msg);
+  this->lastPublication = gazebo::common::Time::GetWallTime();
+}
+
+/////////////////////////////////////////////////
+void ReadyState::DoInitialize()
+{
+  std::cout << "ReadyState::Initialize()" << std::endl;
+}
+
+/////////////////////////////////////////////////
+void ReadyState::DoOnState()
+{
+  if (this->GazeboState() == "gazebo_go")
+    this->plugin.ChangeState(*this->plugin.initCondsState);
+
+  //std::cout << "ReadyState::DoOnState()" << std::endl;
+};
+
+/////////////////////////////////////////////////
+void InitCondsState::DoInitialize()
+{
+  // Send initial conditions.
+  std::cout << "InitCondsState::DoInitialize()" << std::endl;
+  //std::cout << "Initial conditions" << std::endl;
+};
+
+/////////////////////////////////////////////////
+void InitCondsState::DoUpdate()
+{
+  // Check if the initial conditions are satisfied.
+
+  //std::cout << "InitCondsState::DoUpdate()" << std::endl;
+
+  if (this->timer.GetElapsed() >= gazebo::common::Time(2.0))
+    this->plugin.ChangeState(*this->plugin.runningState);
+};
+
+/////////////////////////////////////////////////
+void RunningState::DoInitialize()
+{
+  std::cout << "RunningState::Initialize()" << std::endl;
+}
+
+/////////////////////////////////////////////////
+void RunningState::DoUpdate()
+{
+  // Send the next command.
+
+  //std::cout << "RunningState::DoUpdate()" << std::endl;
+
+  // Check if we are done with the run
+  if (this->timer.GetElapsed() >= gazebo::common::Time(5.0))
+    this->plugin.ChangeState(*this->plugin.endState);
+};
+
+/////////////////////////////////////////////////
+void EndState::DoInitialize()
+{
+  std::cout << "EndState::Initialize()" << std::endl;
+}
+
+/////////////////////////////////////////////////
+void EndState::DoOnState()
+{
+  // Check if Gazebo is ready for another run.
+  if (this->GazeboState() == "gazebo_ready")
+    this->plugin.ChangeState(*this->plugin.readyState);
+
+  //std::cout << "EndState::DoOnState()" << std::endl;
+}
+
+/////////////////////////////////////////////////
+ValidationController::ValidationController()
   : readyState(new ReadyState(kReadyState, *this)),
+    initCondsState(new InitCondsState(kInitCondsState, *this)),
+    runningState(new RunningState(kRunningState, *this)),
+    endState(new EndState(kEndState, *this)),
     currentState(readyState.get())
-  {
-  }
+ {
+ }
 
-  /// \brief ToDo
-  public: ~ValidationController(){};
-
-  /// \brief ToDo
-  public: void Start()
-  {
-    while (true)
-    {
-      if (this->currentState)
-        this->currentState->Update();
-
-      // 1000 Hz.
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-  }
-
-  /// \brief "ready" state.
-  public: std::unique_ptr<ReadyState> readyState;
-
-  /// \brief Pointer to the current game state.
-  public: State *currentState;
+/////////////////////////////////////////////////
+ValidationController::~ValidationController()
+{
 };
+
+/////////////////////////////////////////////////
+void ValidationController::Start()
+{
+  while (true)
+  {
+    if (this->currentState)
+      this->currentState->Update();
+
+    // 1000 Hz.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+}
+
+//////////////////////////////////////////////////
+void ValidationController::ChangeState(State &_newState)
+{
+  // Only update the state if _newState is different than the current state.
+  if (!this->currentState || *this->currentState != _newState)
+  {
+    this->currentState->Teardown();
+    this->currentState = &_newState;
+  }
+}
 
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
