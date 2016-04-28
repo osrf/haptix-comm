224cdfc4cc77faebc9ae8772aff5142a86c89360
Carlos Agüero <caguero@osrfoundation.org>
2016-04-28 11:25:35 -0700

    Tweaking.

--- example/validationController.cc
+++ example/validationController.cc
@@ -15,12 +15,143 @@
  *
 */
 
+#include <iostream>
+#include <fstream>
 #include <gazebo/plugins/validation/ValidationController.hh>
+#include <haptix/comm/haptix.h>
+
+/// \brief
+class WristLimitValidation : public gazebo::ValidationController
+{
+
+  public: WristLimitValidation(const std::string &_filePrefix)
+    : filePrefix(_filePrefix)
+  {
+  }
+
+  //////////////////////////////////////////////////
+  void InitializeInitConds()
+  {
+    hxCommand cmd;
+    hxSensor sensor;
+
+    // Indicate that the positions we set should be used.
+    cmd.ref_pos_enabled = 1;
+    // We're not setting it, so indicate that ref_vel should be ignored.
+    cmd.ref_vel_enabled = 0;
+    // We're not setting it, so indicate that ref_vel_max should be ignored.
+    cmd.ref_vel_max_enabled = 0;
+    // We're not setting it, so indicate that gain_pos should be ignored.
+    cmd.gain_pos_enabled = 0;
+    // We're not setting it, so indicate that gain_vel should be ignored.
+    cmd.gain_vel_enabled = 0;
+    // Set the desired position of this motor
+    cmd.ref_pos[0] =static_cast<float>(0);
+
+    // Send the new joint command and receive the state update.
+    if (hx_update(&cmd, &sensor) != hxOK)
+    {
+      std::cerr << "hx_update(): Request error" << std::endl;
+    }
+  };
+
+  //////////////////////////////////////////////////
+  // void UpdateInitConds()
+  // {
+
+  // };
+
+  //////////////////////////////////////////////////
+  bool Initializing() const
+  {
+    return this->currentState->timer.GetElapsed() < gazebo::common::Time(2.0);
+  }
+
+  //////////////////////////////////////////////////
+  void InitializeRun()
+  {
+    std::string filename = this->filePrefix + std::to_string(this->counter)
+      + ".log";
+    if (this->logFile.is_open())
+      this->logFile.close();
+
+    this->logFile.open(filename);
+
+    ++this->counter;
+    this->step = 0;
+  };
+
+  //////////////////////////////////////////////////
+  void UpdateRun()
+  {
+    hxCommand cmd;
+    hxSensor sensor;
+
+    // Indicate that the positions we set should be used.
+    cmd.ref_pos_enabled = 1;
+    // We're not setting it, so indicate that ref_vel should be ignored.
+    cmd.ref_vel_enabled = 0;
+    // We're not setting it, so indicate that ref_vel_max should be ignored.
+    cmd.ref_vel_max_enabled = 0;
+    // We're not setting it, so indicate that gain_pos should be ignored.
+    cmd.gain_pos_enabled = 0;
+    // We're not setting it, so indicate that gain_vel should be ignored.
+    cmd.gain_vel_enabled = 0;
+    // Set the desired position of this motor
+    cmd.ref_pos[0] =static_cast<float>(
+        350 * 0.5 * sin(0.05 * 2.0 * M_PI * this->step * 0.08));
+
+    // Send the new joint command and receive the state update.
+    if (hx_update(&cmd, &sensor) != hxOK)
+    {
+      std::cerr << "hx_update(): Request error" << std::endl;
+    }
+
+    usleep(20000);
+
+    // Write time in the log file.
+    this->logFile << sensor.time_stamp.sec << "." << sensor.time_stamp.nsec;
+    // Write the state of wrist_rot joint in the log file.
+    this->logFile << " " << sensor.joint_pos[0] << std::endl;
+
+    ++this->step;
+  }
+
+  //////////////////////////////////////////////////
+  bool Running() const
+  {
+    return this->step < 250;
+  }
+
+  /// \brief Run counter.
+  unsigned int counter = 0;
+
+  /// \brief ToDo.
+  int step = 0;
+
+  /// \brief ToDo.
+  std::ofstream logFile;
+
+  /// \brief ToDo.
+  std::string filePrefix;
+};
+
+//////////////////////////////////////////////////
+void usage()
+{
+  std::cout << "validationController <file_prefix>" << std::endl;
+}
 
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-  gazebo::ValidationController controller;
+  if (argc != 2)
+  {
+    usage();
+    return -1;
+  }
+
+  WristLimitValidation controller(argv[1]);
   controller.Start();
 
   return 0;
